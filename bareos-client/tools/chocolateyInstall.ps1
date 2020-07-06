84b97a217eb8942f606c68a4df2ad6468adbd763
sovamakarosh <obe_van@mail.ru>
2020-07-06 19:33:30 +0300

    Ver. 6.6

--- bareos-client/tools/chocolateyInstall.ps1
+++ bareos-client/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'bareos-client'
 $installerType = 'exe'
-$url = 'https://download.bareos.org/bareos/release/17.2/windows/winbareos-17.2.4-postvista-32-bit-r8.1.exe'
-$url64 = 'https://download.bareos.org/bareos/release/17.2/windows/winbareos-17.2.4-postvista-64-bit-r8.1.exe'
+$url = 'https://download.bareos.org/bareos/release/18.2/windows/winbareos-18.2.5-release-32-bit-r127.2.exe'
+$url64 = 'https://download.bareos.org/bareos/release/18.2/windows/winbareos-18.2.5-release-64-bit-r127.2.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
