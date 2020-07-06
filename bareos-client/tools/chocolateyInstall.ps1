7ebe9c7f36b2879fdb4491bfb00470f42e3455f4
sovamakarosh <obe_van@mail.ru>
2020-07-06 19:42:54 +0300

    Ver. 6.6

--- bareos-client/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'bareos-client'
-$installerType = 'exe'
-$url = 'https://download.bareos.org/bareos/release/18.2/windows/winbareos-18.2.5-release-32-bit-r127.2.exe'
-$url64 = 'https://download.bareos.org/bareos/release/18.2/windows/winbareos-18.2.5-release-64-bit-r127.2.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
