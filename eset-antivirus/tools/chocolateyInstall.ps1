0ec3279ce95c52c5b0f53fe08f88040b3213a82a
sovamakarosh <obe_van@mail.ru>
2020-07-13 16:58:29 +0300

    Last ver. 7.2.2055

--- eset-antivirus/tools/chocolateyInstall.ps1
+++ eset-antivirus/tools/chocolateyInstall.ps1
@@ -1,32 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-#[[AutomaticPackageNotesInstaller]]
 $packageName= 'eset-antivirus'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url           = 'https://download.eset.com/com/eset/apps/business/eea/windows/latest/eea_nt32.msi' # download url, HTTPS preferred
-  url64bit      = 'https://download.eset.com/com/eset/apps/business/eea/windows/latest/eea_nt64.msi' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-  #file         = $fileLocation
+  url           = 'https://download.eset.com/com/eset/apps/business/eea/windows/latest/eea_nt32.msi'
+  url64bit      = 'https://download.eset.com/com/eset/apps/business/eea/windows/latest/eea_nt64.msi'
+  checksum      = '1B0E50372A2AD7B752363E30EAA3A83367B1D5A880C4C6338C4E30E91F248141'
+  checksumType  = 'sha256'
+  checksum64    = 'CB0A61ECE14C753074F94FC0302DAD875148391001A43265F52395D918129D1C'
+  checksumType64= 'sha256'
 
-#  softwareName  = '[[PackageName]]*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-#  checksum      = '[[Checksum]]'
-#  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-#  checksum64    = '[[Checksum64]]'
-#  checksumType64= 'sha256'
-
-  silentArgs    = '/qn' # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = '/qn'
   validExitCodes= @(0, 3010, 1641)
 }
 
-#https://chocolatey.org/docs/helpers-install-chocolatey-package
 Install-ChocolateyPackage @packageArgs
-## If you are making your own internal packages (organizations), you can embed the installer or
-## put on internal file share and use the following instead (you'll need to add $file to the above)
-# https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-#Install-ChocolateyInstallPackage @packageArgs
