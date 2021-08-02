623a43453dc55dd65cf74cda06ca48826f7857ac
kilaketia <4190096+kilaketia@users.noreply.github.com>
2021-08-02 10:41:54 +0200

    Fix checksums for 8.1.2031.0

--- eset-antivirus/tools/chocolateyInstall.ps1
+++ eset-antivirus/tools/chocolateyInstall.ps1
@@ -9,9 +9,9 @@ $packageArgs = @{
   fileType      = 'MSI'
   url           = 'https://download.eset.com/com/eset/apps/business/eea/windows/latest/eea_nt32.msi'
   url64bit      = 'https://download.eset.com/com/eset/apps/business/eea/windows/latest/eea_nt64.msi'
-  checksum      = '63DB41DD5400C03A572D9141488B2791D5A807CE8A173A4695F1AAE905939013'
+  checksum      = '057fbab8e49f3f888e61dca7ccd7f19892b25212aa64afa00b5258e43f73f146'
   checksumType  = 'sha256'
-  checksum64    = 'D8CB72E5A921262DD3C1FE8FE172090F46680F0657C250D21727FC5C79E647BF'
+  checksum64    = '1c3ab920c06553350157e4c4e209c28753e3ef496ef593b02b35352ea580186c'
   checksumType64= 'sha256'
 
   silentArgs    = '/qn'
