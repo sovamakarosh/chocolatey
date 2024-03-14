975fda6fbc14bb6703551c4fb5c053ef3e860332
Slava <obe_van@mail.ru>
2024-03-15 01:29:56 +0300

    Add files via upload

--- bareos-client/tools/chocolateyInstall.ps1
+++ bareos-client/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageName = 'bareos-client'
-$installerType = 'exe'
-$url = 'https://download.bareos.org/current/windows/winbareos-23.0.3~pre34.9a6c163ce-release-64-bit.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-$checksum    = 'B1D5F1B7D3D4B10985663BEFCDC60B2A7F4E93E0FE31E17AB815B020B0EBA7C7'
-$checksumType= 'sha256'
+﻿$PackageName = 'bareos-client'
+$InstallerType = 'exe'
+$Url64 = 'https://download.bareos.org/current/windows/winbareos-23.0.3~pre34.9a6c163ce-release-64-bit.exe'
+$SilentArgs = '/S'
+$ValidExitCodes = @(0)
+$Checksum64    = 'B1D5F1B7D3D4B10985663BEFCDC60B2A7F4E93E0FE31E17AB815B020B0EBA7C7'
+$ChecksumType64= 'sha256'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$checksum" "$checksumType"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$PackageName" "$InstallerType" "$SilentArgs" "$Url64" "$Checksum64" "$ChecksumType64"  -validExitCodes $ValidExitCodes
